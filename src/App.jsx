e2148d36685632ff0f106ab91162cc4e59586e5b
JAMALanam <jamalanammi21@gmail.com>
2025-11-08 16:27:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "./index.css";
 import PolkaSwap from "./PolkaSwap-swap-ui";
 
 export default function App() {
   return (
-    <div>
-      <PolkaSwap />
-    </div>
+    <>
+      <div className="particles"></div>
+      <div className="relative z-10">
+        <PolkaSwap />
+      </div>
+    </>
   );
 }
